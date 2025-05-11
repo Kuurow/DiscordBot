69198ded85fb870f71d3f778ee88125fe86a9f4e
Kuurow <kuuroww@gmail.com>
2025-05-11 20:08:38 +0200

    set the basic input for the request in the reply of the info cmd

--- commands/arknights/operatorInfo.js
+++ commands/arknights/operatorInfo.js
@@ -1,44 +1,54 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const axios = require("axios");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
 const { paths } = require('../../constants.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("info")
-        .setDescription("Get Operator's information")
-        .addStringOption(option => option.setName('name').setDescription('Input an operator\'s name').setRequired(true).setAutocomplete(true)),
-    async autocomplete(interaction) {
-        const value = interaction.options.getString('name').toLowerCase();
-        
-    },
+        .setName('info')
+        .setDescription('Get Operator\'s information')
+        .addStringOption((option) =>
+            option
+                .setName('name')
+                .setDescription('Input an operator\'s name')
+                .setRequired(true)
+                .setAutocomplete(false)
+        ),
+    // async autocomplete(interaction) { // list builder for autocompletion
+    //     const value = interaction.options.getFocused().toLowerCase();
+    //     console.log(value);
+    //     const resp2 = await axios.get(`${paths.apiUrl}/operator/match/t?limit=6`);
+    //     console.log(resp2)
+    // },
     async execute(interaction) {
         const client = interaction.client.user;
+        const operatorInput = interaction.options.getString('name').toLowerCase() ?? 'amiya';
+
         await interaction.deferReply();
 
         try {
-            const response = await axios.get(`${paths.apiUrl}/operator/texas`);
+            const response = await axios.get(`${paths.apiUrl}/operator/${operatorInput}`);
             const operator = response.data.value;
- 
+
             const embedMessage = new EmbedBuilder()
-                .setColor("#D5672C")
+                .setColor('#D5672C')
                 .setTitle(`*${operator.data.name}*'s information`)
                 .setAuthor({
                     name: `${client.username}`,
-                    iconURL: `${client.avatarURL()}`
+                    iconURL: `${client.avatarURL()}`,
                 })
                 .setDescription(
                     `There is everything you need to know about the operator **${operator.data.name}**`
                 )
                 .setImage(`${paths.aceshipImageUrl}/characters/${operator.skins[0].portraitId}.png`)
                 .setThumbnail(`${paths.aceshipImageUrl}/avatars/${operator.skins[0].avatarId}.png`)
-                .addFields({ name: "title", value: "value", inline: false })
-                .setFooter({ text: "footer", iconURL: "https://i.imgur.com/wSTFkRM.png" });
+                .addFields({ name: 'title', value: 'value', inline: false })
+                .setFooter({ text: 'footer', iconURL: 'https://i.imgur.com/wSTFkRM.png' });
 
             await interaction.editReply({ embeds: [embedMessage] });
         } catch (error) {
             console.log(error);
             await interaction.editReply({
-                content: "An error occured with the command!",
+                content: 'An error occured with the command!',
                 ephemeral: true,
             });
         }
